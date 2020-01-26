c3bb32d000d8e6f3cb06fe1ed920b6436a9754dd
unknown <jedrzejczyk.pawel@outloog.com>
2020-01-26 13:04:39 +0100

    second one

--- License-plate-detection2.py
+++ License-plate-detection2.py
@@ -2,32 +2,31 @@ import cv2 as cv
 import imutils
 import pytesseract
 import string
-import random
 import numpy as np
 from PIL import Image
-import math as m
+#import math as m
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Python38\\TesseractOCR\\tesseract.exe"
 
 ### Reading Image -> Resiziing it -> copying original image to variable 'image'
-fileDIR = '1m\\8'
+fileDIR = '2m\\1'
 image = cv.imread(fileDIR+'.jpg')
 image = imutils.resize(image, width=1000)
 
 # Changing color scale to gray
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-cv.imshow('1 - Grayscale version', gray)
-cv.waitKey(0)
+# cv.imshow('1 - Grayscale version', gray)
+# cv.waitKey(0)
 
-# Rozmycie
+# Blur
 gray = cv.bilateralFilter(gray,  40, 25, 25)
-cv.imshow("2 - Bilateral version", gray)
-cv.waitKey(0)
+# cv.imshow("2 - Bilateral version", gray)
+# cv.waitKey(0)
 
 # Drawing Contours with Canny algorythm
 edged = cv.Canny(gray, 170,200)
-cv.imshow("3 - Canny",edged)
-cv.waitKey(0)
+# cv.imshow("3 - Canny",edged)
+# cv.waitKey(0)
 
 # Find Countours
 cnts, new = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -35,11 +34,10 @@ cnts, new = cv.findContours(edged.copy(), cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 # Draw Contours
 img1 = image.copy()
 cv.drawContours(img1, cnts,-1,(0,255), 3)
-cv.imshow("4 - Canny All contours",img1)
-cv.waitKey(0)
+# cv.imshow("4 - Canny All contours",img1)
+# cv.waitKey(0)
 
 # Find rectangular shape with adjusted side to side ratio
-idx = 7
 findings = 0
 method = 0
 
@@ -51,22 +49,22 @@ for c in cnts:
         x, y, w, h, =cv.boundingRect(c)
         diagonalpow2 = pow(w,2)+pow(h,2)
 
-        if w > 165 and h > 36 and w > (3 * h) and w < (5 * h) and diagonalpow2 < (pow(h, 2) + pow(w, 2) + 20) and diagonalpow2 > (pow(h, 2) + pow(w, 2) - 20): #Jednorzędowe tablice dla samochodów oraz przyczep	520 x 114 mm =>4:1
+        if w > 165 and h > 36 and w > (3 * h) and w < (5 * h) and w < 300 and h < 50: #Jednorzędowe tablice dla samochodów oraz przyczep	520 x 114 mm =>4:1
             new_img=image[y:y+h, x:x+w]
             print(w, h)
             findings = findings + 1
             method = 1
             name = 'Found plate ' + str(findings)
             print(name)
-            cv.imshow(name, new_img)
-            cv.waitKey(0)
+            # cv.imshow(name, new_img)
+            # cv.waitKey(0)
             cv.imwrite(fileDIR+'_byShape_plate.jpg', new_img)
             break
 #
 # for 1 meter distance  -  if w>300 and h>60 and w > (3* h)and w < (5 * h) and diagonalpow2 < (pow(h,2) + pow(w, 2) + 20) and diagonalpow2 > (pow(h,2) + pow(w, 2) - 20):
 # for 2 meter distance  -  if w>165 and h>36 and w > (3* h)and w < (5 * h) and diagonalpow2 < (pow(h,2) + pow(w, 2) + 20) and diagonalpow2 > (pow(h,2) + pow(w, 2) - 20):
 
-print("Znaleziono ",findings, " elementow za pomoca detekcji ksztaltu")
+#print("Znaleziono ",findings, " elementow za pomoca detekcji ksztaltu")
 findings_color = 0
 
 # if the plates weren't found by shape detection, we try using color detection based on blue color on the left side of the plates
@@ -83,8 +81,8 @@ if findings == 0:
     # Now the do an AND operation using our colorMask
     output = cv.bitwise_and(image, image, mask = colorMask)
 
-    cv.imshow('Color Image', output)
-    cv.waitKey(0)
+    # cv.imshow('Color Image', output)
+    # cv.waitKey(0)
 
     # Shifting the blue areas do gray scale
     grayBlueField = cv.cvtColor(output, cv.COLOR_BGR2GRAY)
@@ -102,10 +100,10 @@ if findings == 0:
         plates_2m = image[y-25:y + 15, x-5:x + 195]
         cv.imwrite(fileDIR + '_byColor_1m_plate.jpg', plates_1m)
         cv.imwrite(fileDIR + '_byColor_2m_plate.jpg', plates_2m)
-        cv.imshow('Plates for 1m distance', plates_1m)
-        cv.waitKey(0)
-        cv.imshow('Plates for 2m distance', plates_2m)
-        cv.waitKey(0)
+        # cv.imshow('Plates for 1m distance', plates_1m)
+        # cv.waitKey(0)
+        # cv.imshow('Plates for 2m distance', plates_2m)
+        # cv.waitKey(0)
         break
 
 # OCR
